7a55b527263510cbe0abc73b7c6e414eaa6a36d3
blockmonkey <blockmonkey@naver.com>
2022-04-02 15:44:22 +0900

    Header

--- block-reviews/src/App.js
+++ block-reviews/src/App.js
@@ -8,24 +8,24 @@ import WriteReview from "./components/review/WriteReview";
 import DetailReview from "./components/review/Detail";
 import DetailStore from "./components/Stores/DetailStore";
 import My from "./components/user/My";
-import AddStore from "./components/stores/AddStore";
+import AddStore from "./components/Stores/AddStore";
+import Header from "./components/Header/Header";
 
 
 function App() {
   return (
-
     <div className='App'>
-          <div className='block-review-banner'>
-                BLOCK REVIEWS
-            </div>
+      <Header />
+        <div className='block-review-banner'>
+            BLOCK REVIEWS
+        </div>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="Register" element={<Register />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/store" element={<AddStore/>}/>
         <Route path="/my/:userId" element={<My />} />
         <Route path="/review/write" element={<WriteReview />} />
-        <Route path="/review/detail" element={<DetailStore />} />
-        <Route path="/review/:reviewId" element={<Register />} />
+        <Route path="/review/:reviewId" element={<DetailStore />} />
       </Routes>
 
     </div>
--- block-reviews/src/components/stores/DetailStore.jsx
+++ block-reviews/src/components/stores/DetailStore.jsx
@@ -1,11 +1,20 @@
 import '../../scss/Login.scss'
 import { Link } from "react-router-dom";
 import { useState } from 'react';
-import github from '../../images/github.png'
+import github from '../../images/github.png';
+import StoreReview from "./sections/StoreReview";
 
 function AddStore() {
     const store = {Name:"가게", description: "설명", cate1:"음식점", cate2:"브런치 카페", thumbnail : {github}, Phone:"0100000000", address:"경기도 성남시 샬라샬라"}
     
+    const garaReview = [
+        {id: 1, title: "이 가게 짱 조아여", description : "이 가게는 ㅇㅇㅇㅇ가 매우 좋고 ~~ 맛도 아주좋고 ~~", liked : 34, earning: 1234324, sale: true },
+        {id: 2, title: "이 가게 짱 조아여", description : "이 가게는 ㅇㅇㅇㅇ가 매우 좋고 ~~ 맛도 아주좋고 ~~", liked : 34, earning: 1234324, sale: true },
+        {id: 3, title: "이 가게 짱 조아여", description : "이 가게는 ㅇㅇㅇㅇ가 매우 좋고 ~~ 맛도 아주좋고 ~~", liked : 34, earning: 1234324, sale: false },
+        {id: 4, title: "이 가게 짱 조아여", description : "이 가게는 ㅇㅇㅇㅇ가 매우 좋고 ~~ 맛도 아주좋고 ~~", liked : 34, earning: 1234324, sale: true },
+        {id: 5, title: "이 가게 짱 조아여", description : "이 가게는 ㅇㅇㅇㅇ가 매우 좋고 ~~ 맛도 아주좋고 ~~", liked : 34, earning: 1234324, sale: false },
+        {id: 6, title: "이 가게 짱 조아여", description : "이 가게는 ㅇㅇㅇㅇ가 매우 좋고 ~~ 맛도 아주좋고 ~~", liked : 34, earning: 1234324, sale: true },
+    ]
 
     const Btn_Create_Click = () => {
         
@@ -19,9 +28,9 @@ function AddStore() {
             <div className='address'>{store.address}</div>
             
             {/* 이곳에 리뷰 등록 폼 / 조회 리스트 쭈욱~ */}
+            <StoreReview garaReview={garaReview}/>
         </div>
     )
 }
 
 export default AddStore;
-
