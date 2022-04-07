be0cc6a683fa822c1b1bfe957fd459d31fe17266
blockmonkey <blockmonkey@naver.com>
2022-04-07 22:54:03 +0900

    Update DetailStore.jsx

--- block-reviews/src/components/Stores/DetailStore.jsx
+++ block-reviews/src/components/Stores/DetailStore.jsx
@@ -6,7 +6,7 @@ import StoreReview from "./sections/StoreReview";
 
 function AddStore(storeId) {
     const store = {Name:"가게", description: "설명", cate1:"음식점", cate2:"브런치 카페", thumbnail : {github}, Phone:"0100000000", address:"경기도 성남시 샬라샬라"}
-}
+
     
     const garaReview = [
         {id: 1, title: "이 가게 짱 조아여", description : "이 가게는 ㅇㅇㅇㅇ가 매우 좋고 ~~ 맛도 아주좋고 ~~", liked : 34, earning: 1234324, sale: true },
