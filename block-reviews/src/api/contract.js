d9bbe5a1870113e3760d792788336ff1b628e26c
blockmonkey <blockmonkey@naver.com>
2022-04-19 22:38:07 +0900

    거래, 판매등록, 철회 API 추가

--- block-reviews/src/api/contract.js
+++ block-reviews/src/api/contract.js
@@ -77,3 +77,43 @@ export const getNftData = async(nftUrl) => {
 }
 
 export const adminPubKey = "0xB28333cab47389DE99277F1A79De9a80A8d8678b";
+
+export const onSale = async(params) => {
+    try {
+        const res = await axios.post(`/blockreview/review/sale/onsale`, {
+            reviewId : params.reviewId,
+            price : params.price,
+            pubkey : params.pubkey,
+            privateKey: params.privatekey,
+        })
+        return res;
+    } catch (err) {
+        throw err;
+    }
+}
+
+export const offSale = async(params) => {
+    try {
+        const res = await axios.post(`/blockreview/review/sale/offsale`, {
+            reviewId : params.reviewId,
+            pubkey : params.pubkey,
+            privateKey: params.privatekey,
+        })
+        return res;
+    } catch (err) {
+        throw err;
+    }
+}
+
+export const trade = async(params) => {
+    try {
+        const res = await axios.post(`/blockreview/review/sale/trade`, {
+            reviewId : params.reviewId,
+            pubkey_buyer: params.pubkey_buyer,
+            privatekey_buyer: params.privatekey_buyer
+        })
+        return res;
+    } catch (err) {
+        throw err;
+    }
+}
